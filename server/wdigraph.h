fa3dad7ff0859ab6e7c25bc35e60083da405b7aa
Bhavnoor Kaur <bhavnoorks@gmail.com>
2021-10-15 13:14:46 -0600

    Changes to the comments

--- server/wdigraph.h
+++ server/wdigraph.h
@@ -1,10 +1,3 @@
-// -----------------------------------------
-// Name: Bhavnoor Kaur
-// ID:1623727
-// CMPUT 275, Winter 2021
-//
-// Assignment 1: Navigation System (Part 2)
-// -----------------------------------------
 #ifndef _WEIGHTED_GRAPH_H_
 #define _WEIGHTED_GRAPH_H_
 
