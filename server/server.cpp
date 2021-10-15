b9fcc4609710ce888ff20d2c1841071eab6e20c9
Bhavnoor Kaur <66292885+Bhavnoor-Kaur@users.noreply.github.com>
2021-10-15 07:46:15 -0600

    Final Update

--- server/server.cpp
+++ server/server.cpp
@@ -1,11 +1,3 @@
-// -----------------------------------------
-// Name: Bhavnoor Kaur
-// ID:1623727
-// CMPUT 275, Winter 2021
-//
-// Assignment 1: Navigation System (Part 2)
-// -----------------------------------------
-
 #include <iostream>
 #include <cassert>
 #include <fstream>
